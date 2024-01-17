80cb777e18dd502a7fc87e2bd67a35e09963a36a
Aabis-Ahmed-Hassan <aabisahmedhassan3@gmail.com>
2024-01-17 08:57:17 +0500

    Done with Provider Basic Example

--- lib/Provider/count_provider.dart
+++ lib/Provider/count_provider.dart
@@ -3,4 +3,17 @@ import 'package:flutter/foundation.dart';
 
 class CountClass with ChangeNotifier{
 
+  int countVariable = 0;
+  int get countGetter => countVariable;
+
+
+
+
+  void setCount() {
+
+    countVariable++;
+    notifyListeners();
+  }
+
+
 }
--- lib/Screens/count_example.dart
+++ lib/Screens/count_example.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_state_management/Provider/count_provider.dart';
 
 class CountWidget extends StatefulWidget {
   const CountWidget({super.key});
@@ -8,8 +12,23 @@ class CountWidget extends StatefulWidget {
 }
 
 class _CountWidgetState extends State<CountWidget> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    final countObjectForTimer = Provider.of<CountClass>(context , listen:false);
+    Timer.periodic(Duration(seconds: 1) , (timer){
+
+      countObjectForTimer.setCount();
+
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final countObject = Provider.of<CountClass>(context , listen:false);
+    print('build');
     return Scaffold(
       appBar: AppBar(
         title: Text('Count Screen'),
@@ -22,12 +41,20 @@ class _CountWidgetState extends State<CountWidget> {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Center(
-            child: Text('0'),
+            child: Consumer<CountClass>(
+              builder: (context ,value, child) {
+                return Text(value.countGetter.toString(), style:TextStyle(fontSize: 50),);
+              }
+            ),
           ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          countObject.setCount();
+
+
+        },
         child: Icon(Icons.add),
         backgroundColor: Colors.teal,
       ),
