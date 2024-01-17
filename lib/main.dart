764900277ad2d7c08a5a3bcd8fc3c1d8347dd2d5
Aabis-Ahmed-Hassan <aabisahmedhassan3@gmail.com>
2024-01-17 22:11:35 +0500

    Previous Commit was a mistake

--- lib/main.dart
+++ lib/main.dart
@@ -1,104 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_state_management/Provider/count_provider.dart';
+import 'package:provider_state_management/Provider/favourite_class.dart';
+import 'package:provider_state_management/Provider/my_provider_class.dart';
+import 'package:provider_state_management/Provider/my_slider_screen.dart';
+import 'package:provider_state_management/Screens/favourite_screen.dart';
+import 'package:provider_state_management/Screens/my_provider_screen.dart';
+import 'package:provider_state_management/Screens/my_slider_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  var blackTheme = false;
-
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        primaryColor: Colors.blue,
-        brightness: blackTheme ? Brightness.dark : Brightness.light,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('My App'),
-          centerTitle: true,
-          actions: [
-            Switch(
-              value: blackTheme,
-              onChanged: (value) {
-                blackTheme = value;
-                setState(() {});
-              },
-            ),
-            const SizedBox(
-              width: 10,
-            ),
-          ],
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_)=>CountClass(),
+
         ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const SizedBox(
-                  height: 50,
-                ),
-                const TextField(
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    label: Text('Weight in Kgs'),
-                  ),
-                ),
-                const TextField(
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    label: Text('Height in Feet'),
-                  ),
-                ),
-                const TextField(
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    label: Text('Height in Inches'),
-                  ),
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-                Container(
-                  height: 50,
-                  width: 250,
-                  decoration: BoxDecoration(
-                    color:Colors.green,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: const Center(
-                    child: Text(
-                      'Calculate',
-                      style: TextStyle(fontSize: 24),
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-              ],
-            ),
-          ),
+        ChangeNotifierProvider(
+          create: (_)=>MyProviderClass(),
+
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: const Icon(
-            Icons.add,
-          ),
+        ChangeNotifierProvider(
+          create: (_)=>MySliderClass(),
+
         ),
+
+        ChangeNotifierProvider(
+            create: (_) => FavouriteClass()
+        )
+
+
+      ],
+
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: FavouriteWidget(),
       ),
     );
+
+
+
   }
 }
+
+
+
